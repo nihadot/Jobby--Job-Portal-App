ff2eed7289c06852b0e970f3240965d05420034c
Alen John <82397801+alenjohn05@users.noreply.github.com>
2021-09-25 17:43:15 +0530

    Update index.js

--- src/components/jobDetails/index.js
+++ src/components/jobDetails/index.js
@@ -207,7 +207,7 @@ class JobDetail extends Component {
 
       <h1>Oops! Something Went Wrong</h1>
       <p>We cannot seem to find the page you are looking for</p>
-      <button className="btn" type="button" onClick={this.getProfile()}>
+      <button className="btn" type="button" onClick={this.getProfile}>
         Retry
       </button>
     </div>
